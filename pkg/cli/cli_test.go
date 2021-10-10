f145e615c90395f1faeb7cdfafc21bd535fb130c
ZOrfeas <orfeas.zografos@gmail.com>
2021-10-10 17:48:09 +0300

    Turn example into test

--- pkg/cli/cli_test.go
+++ pkg/cli/cli_test.go
@@ -1,15 +1,18 @@
 package cli
 
 import (
-	"fmt"
 	"os"
+	"strings"
+	"testing"
 )
 
 // Showcases a simple usecase of the Cli package
-func ExampleCli_Run() {
+func TestCli_Run(t *testing.T) {
 	testCli := NewCli(os.Args[0], "Just a simple example")
+	bld := strings.Builder{}
 	testCallback := func(args []string) error {
-		fmt.Println(args)
+		bld.WriteString(strings.Join(args, " "))
+		bld.WriteString("-")
 		return nil
 	}
 	testCli.AddCommand("cmd1", "This is cmd1", testCallback)
@@ -17,8 +20,7 @@ func ExampleCli_Run() {
 	testCli.AddCommand("cmd3", "This is cmd3", testCallback)
 
 	testCli.Run([]string{"cmd1", "trsh11", "trsh12", "cmd2", "cmd3", "trsh31"}, testCli.Help)
-	// Output:
-	// [trsh11 trsh12]
-	// []
-	// [trsh31]
+	if str := bld.String(); str != "trsh11 trsh12--trsh31-" {
+		t.Fatal("Did not match: ", str)
+	}
 }
